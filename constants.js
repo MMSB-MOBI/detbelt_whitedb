37fb6784e3ae24283d7d9eeca8e261353b970f0f
cecilpert <cecile.hilpert@gmail.com>
2024-01-12 11:38:47 +0100

    update db

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const DB_DIR = "/data/databases/detbelt/proteins"
-const DB_JSON_NAME = "tingoWhite_20210114-102025.json"
-const PDB_DIR = "/data/databases/detbelt/proteins/opmPDB_20210114-102025/validated/"
+const DB_JSON_NAME = "tingoWhite_20230718-145252.json"
+const PDB_DIR = "/data/databases/detbelt/proteins/opmPDB_20230718-145252/validated/"
 
 module.exports = {
     DB_DIR : DB_DIR,
